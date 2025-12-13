6066256475ed9d25d36f43b9c1635e6a6d8f3d2e
akhilthirunalveli <thirunalveliakhil@gmail.com>
2025-12-13 21:15:03 +0530

    chore: initialize react frontend with routing and axios setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Sweets from './pages/Sweets';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Sweets />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ Frontend/src/pages/Sweets.jsx
@@ -0,0 +1,5 @@
+function Sweets() {
+    return <h2>Sweets List</h2>;
+}
+
+export default Sweets;
