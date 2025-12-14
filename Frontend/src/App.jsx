288605e3e1ddd3cecc50a0951578020c8b7e331b
akhilthirunalveli <thirunalveliakhil@gmail.com>
2025-12-14 16:34:20 +0530

    feat(app): setup routing for auth and sweets

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,24 +2,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Sweets from './pages/Sweets';
-import ProtectedRoute from './auth/ProtectedRoute';
+import Navbar from './components/Navbar';
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-
-                <Route
-                    path="/"
-                    element={
-                        <ProtectedRoute>
-                            <Sweets />
-                        </ProtectedRoute>
-                    }
-                />
-            </Routes>
+            <div className="">
+                <Navbar />
+                <div className="pt-16">
+                    <Routes>
+                        <Route path="/" element={<Sweets />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </Routes>
+                </div>
+            </div>
         </BrowserRouter>
     );
 }
